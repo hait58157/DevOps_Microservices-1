671f02f4f961454f421016a80b7d663cc309d4b5
hait58157 <130881061+hait58157@users.noreply.github.com>
2023-05-18 18:12:46 +0700

    a
    a

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+docker_path=haitv97
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl create deploy project-ml-microservice-kubernetes --image="$docker_path/devops"
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward deployment.apps/project-ml-microservice-kubernetes 8000:80
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,25 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-
+docker_path=haitv97
+source .env
 # Step 2:  
 # Authenticate & tag
-echo "Docker ID and Image: $dockerpath"
+echo "Docker ID and Image: $docker_path"
+echo "$MY_PASSWORD" | docker login --username $docker_path --password-stdin
+image_tagged=$(docker image list --filter=reference="$docker_path/devops" | grep 'devops' | xargs)
+if [[ -n $image_tagged ]]; then
+	echo "Image already tagged, remove the tagged image."
+	name=$(echo "$image_tagged" | cut -f 1 -d " ")
+	tag=$(echo "$image_tagged" | cut -f 2 -d " ")
+	docker image remove --force "$name":"$tag"
+fi
 
+image_info=$(docker image list | grep 'devops' | xargs)
+image_name=$(echo "$image_info" | cut -f 1 -d " ")
+image_tag=$(echo "$image_info" | cut -f 2 -d " ")
+docker image tag "$image_name:$image_tag" "$docker_path/$image_name:$image_tag"
+docker image list --filter=reference="$docker_path/devops"
 # Step 3:
 # Push image to a docker repository
+docker image push "$docker_path/devops:$image_tag"
